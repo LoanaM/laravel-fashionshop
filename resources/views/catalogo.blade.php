a378e990f34b73970ce5cf2ff15f8e4e1ba2d50e
LoanaM <loanawitch@live.com>
2018-06-11 23:05:36 +0200

    commit01

---
+++ resources/views/catalogo.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+
+@section('main')
+    <div class="container">
+      <div class="presentation">
+        <h1>descrizione sessione</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo conseatur. borum.</p>
+
+      </div>
+      <div class="wrapper">
+        <div class="imgcontainer"></div>
+        <div class="imgcontainer"></div>
+        <div class="imgcontainer"></div>
+        <div class="imgcontainer"></div>
+        <div class="imgcontainer"></div>
+      </div>
+    </div>
+@endsection
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title></title>
+  </head>
+  <body>
+
+    <header>
+      <div class="searchbar">
+        <div class="promotext">
+          Sconti fino al 50% per un periodo limitato!
+        </div>
+        <div class="account">
+          Login
+          Carrello
+          <input type="text" placeholder="Cerca" value="">
+        </div>
+      </div>
+      <div class="navbar">
+        <div class="logo">
+          MBK
+        </div>
+        <div class="menu">
+          HOME WOMAN MAN ACCESSORIES
+        </div>
+      </div>
+    </header>
+
+    <main>
+      @yield('main')
+    </main>
+
+  </body>
+</html>
